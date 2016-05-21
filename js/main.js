940f6a59fd7e2e0ff44f57ac71c450a770696abb
kepocnhh <kepocnhh@gmail.com>
2016-05-21 18:46:57 +0300

    recreate vew system + add projects to base

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 /* globals angular */
-(function () {
-  angular.module('StanLinksApp', [])
-    .controller('StanLinksCtrl', ['$scope', '$http', function ($scope, $http) {
-      $http({
-        method: 'GET',
-        url: 'projects.json'
-      }).then(function successCallback (response) {
-        $scope.projects = response.data
-      })
-    }])
-})()
+(function ()
+{
+	angular.module('StanLinksApp', [])
+		.controller('StanLinksCtrl', ['$scope', '$http', function ($scope, $http)
+		{
+			$http({
+				method: 'GET',
+				url: 'projects.json'
+			}).then(function successCallback (response)
+			{
+				$scope.projects = response.data;
+      		})
+    	}])
+}
+)()
